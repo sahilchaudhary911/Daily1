6e6dd3ca7f71aad4167077794b82f88be50c753d
sahilchaudhary911 <sahilchaudhary2052@gmail.com>
2021-07-13 16:21:46 +0545

    2

---
+++ 2.py
@@ -0,0 +1,8 @@
+# Write a program that asks the user for their name and greets them with their name.
+
+def greeting_user():
+    name = input("Enter your name: ")
+    print(f"Hello {name}!")
+
+
+greeting_user()
