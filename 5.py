4d8b05d12375e0c31ef750f56bba512092ff15ca
sahilchaudhary911 <sahilchaudhary2052@gmail.com>
2021-07-16 10:59:22 +0545

    5.1

--- 5.py
+++ 5.py
@@ -7,7 +7,7 @@ def sum_of_n_num_and_multiple_of_3_and_5():
     for i in range(n_number + 1):
         if i % 3 == 0 or i % 5 == 0:
             sum_n += i
-    print(f"The sum of natural numbers till {n_number} is {sum_n}")
+    print(f"The sum of natural numbers till {n_number} which is also the multiple of 3 or 5 is {sum_n}")
 
 
 sum_of_n_num_and_multiple_of_3_and_5()
